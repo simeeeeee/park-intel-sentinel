9f718a5b0765ee79e0e2b16f83ac9c47e4e2930b
simeeeeee <whdk2340@gmail.com>
2025-08-01 11:23:20 +0900

    fix: robot_id 파라미터 추가

--- backend/app/models/robot.py
+++ backend/app/models/robot.py
@@ -6,5 +6,6 @@ class VehicleInfo(BaseModel):
     ev: str
 
 class RobotStatusRequest(BaseModel):
+    robot_id: Optional[int] = None  # 있어도 되고 없어도 됨
     rfid: str
     vehicles: Dict[str, VehicleInfo]  # 1, 2, 3, ... 키가 string
